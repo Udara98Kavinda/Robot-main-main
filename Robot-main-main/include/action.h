637d09f22db5906633b9a808f3fb2fbfd30b4b84
Udara Kavinda <85794559+Udara98Kavinda@users.noreply.github.com>
2025-08-10 19:49:27 +0530

    Update action.h

--- Robot-main-main/include/action.h
+++ Robot-main-main/include/action.h
@@ -2,7 +2,14 @@
 #define ACTION_H
 
 #include <Arduino.h>
+#include <Motor_control.h>
+#include <Get_readings.h>
+#include <encoder.h>
+#include <ir_detect.h>
 
-void handleSpecialCross(int crossType, const int analog_readings[9], const int thresholds[9]);
+void handleCrossApproach();
+void handleSpecialCross(const char* crossType, const int analog_readings[9], const int thresholds[9]);
+void rearrangeCrossArray();
+void reverseAndSwapSortedArray(const char* arraysorted, int length);
 
 #endif // ACTION_H
